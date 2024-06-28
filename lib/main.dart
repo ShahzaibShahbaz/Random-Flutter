206c839e8bfb4818cf357a6e08907be15eb2caa8
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-06-28 15:55:29 +0300

    Updated main.dart
    added some text and containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 //The main function which is the starting point of all our apps.
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,31 +19,64 @@ class MyApp extends StatelessWidget {
 
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
-          title: Center(child: Text('MY App', style: TextStyle(color: Colors.white),),),
+          title: Center(child: Text('My Profile', style: TextStyle(color: Colors.white),),),
         ),
         backgroundColor: Colors.tealAccent,
         body: Center(
           child: SafeArea(
             child: Center(
               child: Column(
-
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   CircleAvatar(
                     backgroundImage: AssetImage('images/Portrait.jpeg'),
-
                     radius: 50,
                     backgroundColor: Colors.blue,
-
                   ),
+
                   SizedBox(
                     height: 10,
                   ),
 
+                  Text('shahzaib shahbaz', style: TextStyle(fontSize: 48),),
 
 
 
-                  Text('shahzaib shahbaz', style: TextStyle(fontSize: 48),)
+                  Text('Computer Scientist', style: TextStyle(fontWeight: FontWeight.w300, fontSize: 20),),
+
+SizedBox(height: 10,),
+
+                  Container(
+                    color: Colors.white,
+                    height: 50,
+                    width: 400,
+                    padding: EdgeInsets.only(left: 15),
+
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone, size: 35, color: Colors.tealAccent,),
+                        SizedBox(width: 20,),
+                        Text('+923 211896779', style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w500),),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 20,),
+
+                  Container(
+                    color: Colors.white,
+                    height: 50,
+                    width: 400,
+                    padding: EdgeInsets.only(left: 15),
+                    child:
+                    Row(
+                      children: [
+                        Icon(Icons.email_rounded, size: 35, color: Colors.tealAccent,),
+                        SizedBox(width: 20,),
+                        Text('shahzaibshahbaz@gmail.com', style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w500, ),)
+                      ],
+                    ),
+                  )
 
 
                 ],
